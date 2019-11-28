f33a443e05bc10fc7459190f47cd61a2060d1f37
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2019-11-28 10:54:29 +0100

    Add keyboard enter/leave message

--- wayland-input.c
+++ wayland-input.c
@@ -65,10 +65,10 @@ static void keyboard_keymap (void *data, struct wl_keyboard *keyboard, uint32_t 
 	xkb_state = xkb_state_new (keymap);
 }
 static void keyboard_enter (void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys) {
-	
+	printf ("keyboard enter\n");
 }
 static void keyboard_leave (void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface) {
-	
+	printf ("keyboard leave\n");
 }
 static void keyboard_key (void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state) {
 	if (state == WL_KEYBOARD_KEY_STATE_PRESSED) {
